f2dc43c5d96b0b636bbcbada376196e0195d5c1e
Liam Bonnage <lbonnage@gmail.com>
2020-05-18 11:44:49 -0400

    Initial commit, removed the subrepository that was causing issues

---
+++ web/server.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request, url_for, redirect
+# from flask_cors import CORS
+
+# Set the project root directory as the website's static folder
+app = Flask(__name__,
+			static_url_path='',
+			static_folder='web/build',
+			template_folder='web/build')
+
+# Enable for local testing
+# CORS(app)
+
+
+@app.route('/')
+def root():
+	return render_template('index.html')
+
+
+if __name__ == '__main__':
+	app.run()
