f767ae523d55ed556d184b528d687fd37bc27335
Liam Bonnage <lbonnage@gmail.com>
2020-05-19 20:29:35 -0400

    Second attempt to fix white screen

---
+++ web/build/precache-manifest.e03fce3eb3a74050825d3744f436dda7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce5c54c6909bb1888cf2f05c8ebdabd7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22048ec3f27a5ecd84f1",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "8e583938a69d83e89e0a",
+    "url": "/static/css/main.c847f8b0.chunk.css"
+  },
+  {
+    "revision": "22048ec3f27a5ecd84f1",
+    "url": "/static/js/2.29ae1060.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.29ae1060.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e583938a69d83e89e0a",
+    "url": "/static/js/main.0083e838.chunk.js"
+  },
+  {
+    "revision": "73f94224884e0ba06838",
+    "url": "/static/js/runtime-main.df8613b9.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
